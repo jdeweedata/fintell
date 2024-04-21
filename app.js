1760ceed0626b6c00f6765b9903166835f653ac4
Jeffrey <jdewee@live.com>
2024-04-21 20:07:17 +0200

    Merge remote changes and add local changes

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const express = require('express');
-const axios = require('axios');
-const nodemailer = require('nodemailer');
-
-const app = express();
-app.use(express.json());
-
 app.post('/analyze', async (req, res) => {
-  const { companyName, sections, words, email } = req.body;
-
+  const { stock, email } = req.body;
   try {
-    await axios.post('https://n8n-render-z96v.onrender.com/webhook-test/3d721320-0385-4895-8a8f-ae55a0c52dbc', {
-      companyName,
-      sections,
-      words,
+    await axios.post('https://n8n-render-z96v.onrender.com/webhook/stockAnalysis', {
+      stock,
       email
     });
-
     res.sendStatus(200);
   } catch (error) {
     console.error('Error executing n8n workflow:', error);
     res.sendStatus(500);
   }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
